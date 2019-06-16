640a97ea1983a0aeacf5f9c424eb4a755b222025
649 <649@users.noreply.github.com>
2019-06-15 18:44:53 -0700

    v1.1
    Couple bug fixes in version 1.1
    
    - No more crashing due to venmo tx msg containing emoji
    - Venmo API no longer responds to limit param
    - Venmo API tx list limited to 20 per request (if you can pull more than
    20, remove the 'limit' variable)

--- Vendont.py
+++ Vendont.py
@@ -3,6 +3,8 @@ from urllib.request import urlopen
 import time
 import json
 import datetime
+import sys
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 class color:
     HEADER = '\033[0m'
@@ -15,7 +17,7 @@ logo = color.HEADER + '''
     ╚████╔╝ ███████╗██║ ╚████║██████╔╝╚██████╔╝██║ ╚████║   ██║   
      ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═══╝   ╚═╝               
                           Author: @037
-                          Version: 1.0
+                          Version: 1.1
 
 #######################################################################
 | Vendont is a Venmo transaction finder/scraper. It uses Venmo's own  |
@@ -35,11 +37,16 @@ date_time2 = input('Enter the date where transactions stop [DD.MM.YYYY HH:mm:SS]
 epoch2 = int(time.mktime(time.strptime(date_time2, pattern)))
 print('[*] Transformed "end" date to EPOCH: %s (local time zone)' %epoch2)
 
-limit = input('Enter the maximum amount transactions to collect: ')
+limit = int(input('Enter the maximum amount transactions to collect (default/max is 20): ')) or 20
 print('')
+if(limit > 20):
+    limit = 20
 vjson = urlopen("https://venmo.com/api/v5/public?since=%s&until=%s&limit=%s" %(epoch, epoch2, limit))
 obj = json.load(vjson)
 #print(obj["data"])
-limit = int(limit)
-for i in range(0, limit):
-    print('ID: %s - %s (%s) sent $ to %s (%s) with message: [%s]'% (obj["data"][i]["payment_id"], obj["data"][i]["actor"]["name"], obj["data"][i]["actor"]["username"], obj["data"][i]["transactions"][0]["target"]["name"], obj["data"][i]["transactions"][0]["target"]["username"], obj["data"][i]["message"]))
+i = 0
+while i < limit and obj["data"][i] != None:
+    if((obj["data"][i]["payment_id"] is not None) and (obj["data"][i]["actor"]["name"] is not None) and (obj["data"][i]["actor"]["username"] is not None) and (obj["data"][i]["transactions"][0]["target"]["name"] is not None) and (obj["data"][i]["transactions"][0]["target"]["username"] is not None) and (obj["data"][i]["message"] is not None)):
+        x = 'ID: %s - %s (%s) sent $ to %s (%s) with message: [%s]'% (obj["data"][i]["payment_id"], obj["data"][i]["actor"]["name"], obj["data"][i]["actor"]["username"], obj["data"][i]["transactions"][0]["target"]["name"], obj["data"][i]["transactions"][0]["target"]["username"], obj["data"][i]["message"])
+        print(x.translate(non_bmp_map))
+        i+=1
